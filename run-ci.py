fc78e3094cf36f3d4304e60346f183de52d0d1b1
Théo DELRIEU <theo.delrieu@tanker.io>
2019-08-21 17:18:37 +0200

    deps: bump native

--- run-ci.py
+++ run-ci.py
@@ -16,7 +16,7 @@ import ci.tanker_configs
 import cli_ui as ui
 from path import Path
 
-DEPLOYED_TANKER = "tanker/2.1.0-beta1@tanker/stable"
+DEPLOYED_TANKER = "tanker/2.1.0-beta2@tanker/stable"
 LOCAL_TANKER = "tanker/dev@tanker/dev"
 
 ARCHS = ["armv7", "armv7s", "armv8", "x86", "x86_64"]
