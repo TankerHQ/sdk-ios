433827f345b8e3e79eaed096e29d7b1b69547ea7
Théo DELRIEU <theo.delrieu@tanker.io>
2019-08-22 12:25:21 +0200

    fix warning

--- Tanker/Headers/TKRInputStreamDataSource+Private.h
+++ Tanker/Headers/TKRInputStreamDataSource+Private.h
@@ -11,7 +11,8 @@
 + (nullable instancetype)inputStreamDataSourceWithCStream:(nonnull tanker_stream_t*)stream
                                               asyncReader:(nonnull TKRAsyncStreamReader*)reader;
 
-- (nullable instancetype)initWithCStream:(nonnull tanker_stream_t*)stream asyncReader:(nonnull TKRAsyncStreamReader*)reader;
+- (nullable instancetype)initWithCStream:(nonnull tanker_stream_t*)stream
+                             asyncReader:(nonnull TKRAsyncStreamReader*)reader;
 
 @property(nullable) tanker_stream_t* stream;
 @property(nonnull) TKRAsyncStreamReader* reader;
