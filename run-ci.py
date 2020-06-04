bbc06cbfe6b08e1d713852a0229aba57e5225845
jerem <jeremy.tellaa@tanker.io>
2020-06-04 16:59:37 +0200

    chore(dep): bump tanker version

--- run-ci.py
+++ run-ci.py
@@ -15,7 +15,7 @@ import ci.tanker_configs
 import cli_ui as ui
 from path import Path
 
-DEPLOYED_TANKER = "tanker/2.4.0-beta3@tanker/stable"
+DEPLOYED_TANKER = "tanker/2.4.0-beta4@tanker/stable"
 LOCAL_TANKER = "tanker/dev@tanker/dev"
 
 ARCHS = ["armv7", "armv7s", "armv8", "x86", "x86_64"]
