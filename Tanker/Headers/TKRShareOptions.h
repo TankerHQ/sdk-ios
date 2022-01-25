fa44b4b22cff552e727103469b55a0676551556d
Philippe Daouadi <philippe.daouadi@tanker.io>
2022-01-25 05:45:17 -0800

    Remove deprecated TKRShareOptions

--- Tanker/Headers/TKRShareOptions.h
+++
@@ -1,6 +0,0 @@
-#warning "This header is deprecated, include TKRSharingOptions.h and use TKRSharingOptions"
-
-#import <Tanker/TKRSharingOptions.h>
-
-// TKRShareOptions is deprecated and has been renamed to TKRSharingOptions
-__attribute__((deprecated)) typedef TKRSharingOptions TKRShareOptions;
