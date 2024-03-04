20325c0047ceb6ad1724574a42808b5f7d371a09
Timothée Isnard <timothee.isnard@tanker.io>
2024-03-04 16:50:20 +0100

    feat: TKRStreamsFromNative, which replaces InputStreamDataSource
    InputStreamDataSource is a POS DataSource,
    used with POSBlobInputStream.
    
    TKRStreamsFromNative is a subclass of TKRStreamBase instead.

---
+++ Tanker/Headers/TKRStreamsFromNative+Private.h
@@ -0,0 +1,15 @@
+#import <Foundation/Foundation.h>
+
+#import <Tanker/TKRAsyncStreamReader+Private.h>
+#import <Tanker/TKRStreamBase.h>
+
+#include "ctanker/stream.h"
+
+@interface TKRStreamsFromNative : TKRStreamBase
+
++ (nullable instancetype)streamsFromNativeWithCStream:(nonnull tanker_stream_t*)cstream
+                                          asyncReader:(nonnull TKRAsyncStreamReader*)reader;
+
+- (nullable instancetype)initWithCStream:(nonnull tanker_stream_t*)cstream
+                             asyncReader:(nonnull TKRAsyncStreamReader*)reader;
+@end
